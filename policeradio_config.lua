209ec7af388732730cc5ed25ff94aae1499be816
Georgy <95245162+Dies63@users.noreply.github.com>
2024-03-03 16:58:35 +0200

    Add files via upload

---
+++ policeradio_config.lua
@@ -0,0 +1,28 @@
+POLICE_RADIO_CONFIG = {}
+
+// Police team
+POLICE_RADIO_CONFIG.TEAMS = {
+	// 1-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX},
+	// 2-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX,},
+	// 3-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX},
+	// 4-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX},
+	// 5-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX},
+	// 6-ая группа
+	{TEAM_XXX, TEAM_XXX},
+	// 7-ая группа
+	{TEAM_XXX, TEAM_XXX},
+	// 8-ая группа
+	{TEAM_XXX, TEAM_XXX},
+	// 9-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX},
+	// 10-ая группа
+	{TEAM_XXX, TEAM_XXX, TEAM_XXX, TEAM_XXX}
+}
+
+// Button for enable/disable radio
+POLICE_RADIO_CONFIG.turnradio = KEY_K
